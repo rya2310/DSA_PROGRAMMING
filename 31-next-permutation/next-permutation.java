8f5c80368de32366cae05c5d0e7f94e366176381
rya2310 <aryaatole689@gmail.com>
2026-02-02 23:42:07 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (12.00%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -3,7 +3,6 @@ class Solution {
         int n = nums.length;
         int pivot = -1;
 
-        // 1️⃣ Find pivot
         for (int i = n - 1; i > 0; i--) {
             if (nums[i - 1] < nums[i]) {
                 pivot = i - 1;
@@ -11,7 +10,7 @@ class Solution {
             }
         }
 
-        // 2️⃣ If pivot exists, swap with next greater
+
         if (pivot != -1) {
             for (int i = n - 1; i > pivot; i--) {
                 if (nums[i] > nums[pivot]) {
@@ -21,18 +20,17 @@ class Solution {
             }
         }
 
-        // 3️⃣ Reverse suffix
+
         reverse(pivot + 1, n - 1, nums);
     }
 
-    // Swap array elements
+
     private void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
     }
 
-    // Reverse subarray
     private void reverse(int i, int j, int[] nums) {
         while (i < j) {
             swap(nums, i, j);
