7690901498dbc8e85fd8829dbc99b5f31fa4f0cb
rya2310 <aryaatole689@gmail.com>
2026-02-26 11:36:12 +0530

    Time: 1 ms (98.53%) | Memory: 47.7 MB (21.77%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -1,6 +1,51 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-    //     int n = nums.length;
+        // //O(n) + extra space
+        // int n = nums.length;
+
+        // int[] prefix = new int[n];
+        // int[] suffix = new int[n];
+
+        // prefix[0] = 0;
+        // suffix[n-1] = 0;
+        // for(int i = 1 ; i < n ; i++){
+        //     prefix[i] = prefix[i-1] + nums[i-1];
+        //     System.out.print(prefix[i]+" ");
+        // }
+        // System.out.println();
+        // for(int i = n-2 ; i >= 0 ; i--){
+        //     suffix[i] = suffix[i+1]+nums[i+1];
+        //     System.out.print(suffix[i]+" ");
+        // }
+
+        // for(int i = 0 ; i < n ; i++){
+        //     if(suffix[i] == prefix[i]) return i;
+        // }
+
+        // return -1;
+
+
+        //O(n) no extra space
+        int n = nums.length;
+        int t_sum = nums[0];
+        for(int i = 1 ; i < n ; i++){
+            t_sum += nums[i];
+        }
+
+        int l_sum = 0;
+
+        for(int i = 0 ; i < n ; i++){
+            int r_sum = t_sum - l_sum - nums[i];
+
+            if(r_sum == l_sum) return i;
+
+            l_sum += nums[i];
+        }
+        return -1;
+    }
+}
+
+//     int n = nums.length;
 
     //     int totalSum = nums[0];
     //     for(int t = 1 ; t < n ; t++){
@@ -20,28 +65,3 @@ class Solution {
     //     }
 
     //     return -1;
-
-        int n = nums.length;
-
-        int[] prefix = new int[n];
-        int[] suffix = new int[n];
-
-        prefix[0] = 0;
-        suffix[n-1] = 0;
-        for(int i = 1 ; i < n ; i++){
-            prefix[i] = prefix[i-1] + nums[i-1];
-            System.out.print(prefix[i]+" ");
-        }
-        System.out.println();
-        for(int i = n-2 ; i >= 0 ; i--){
-            suffix[i] = suffix[i+1]+nums[i+1];
-            System.out.print(suffix[i]+" ");
-        }
-
-        for(int i = 0 ; i < n ; i++){
-            if(suffix[i] == prefix[i]) return i;
-        }
-
-        return -1;
-    }
-}
