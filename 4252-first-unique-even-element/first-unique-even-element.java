161dc2e299892ecb18ec138ecb534330b291745c
rya2310 <aryaatole689@gmail.com>
2026-03-15 07:58:48 +0530

    Time: 1 ms (100.00%) | Memory: 46.4 MB (50.00%) - LeetSync

--- 4252-first-unique-even-element/first-unique-even-element.java
+++ 4252-first-unique-even-element/first-unique-even-element.java
@@ -1,19 +1,24 @@
 class Solution {
     public int firstUniqueEven(int[] nums) {
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int i = 0 ; i < nums.length ; i++){
-            if(nums[i] % 2 == 0){
-                map.put(nums[i] , map.getOrDefault(nums[i],0)+1);
-            }
-        }
+        // Map<Integer,Integer> map = new HashMap<>();
+        // for(int i = 0 ; i < nums.length ; i++){
+        //     if(nums[i] % 2 == 0){
+        //         map.put(nums[i] , map.getOrDefault(nums[i],0)+1);
+        //     }
+        // }
 
-        for(int i= 0 ; i < nums.length ; i++){
-            if(map.containsKey(nums[i])){
-                if(map.get(nums[i]) == 1){
-                    return nums[i];
-                }
-            }
-        }
+        // for(int i= 0 ; i < nums.length ; i++){
+        //     if(map.containsKey(nums[i])){
+        //         if(map.get(nums[i]) == 1){
+        //             return nums[i];
+        //         }
+        //     }
+        // }
+        // return -1;
+
+        int[] freq = new int[101];
+        for(int i : nums) if(i%2 == 0) freq[i]++;
+        for(int i : nums) if(i%2 == 0 && freq[i] == 1) return i;
         return -1;
     }
 }
