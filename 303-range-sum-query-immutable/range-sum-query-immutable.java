e527af51eeb894814c0aa7495e8f365c7098a0d9
rya2310 <aryaatole689@gmail.com>
2026-03-17 06:16:02 +0530

    Time: 9 ms (25.43%) | Memory: 51.1 MB (5.78%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.java
+++ 303-range-sum-query-immutable/range-sum-query-immutable.java
@@ -1,21 +1,34 @@
 class NumArray {
-    int[] prefix;
-
+    int[] segment;
+    int n;
     public NumArray(int[] nums) {
-        int n = nums.length;
-        prefix = new int[n];
-
-        prefix[0] = nums[0];
-
-        for(int i = 1; i< n ; i++){
-            prefix[i] = nums[i]+ prefix[i-1]; 
-        }
+        n = nums.length;
+        segment = new int[4*n];
+        buildSegmentTree(0 , 0 , n-1 , nums);
     }
     
     public int sumRange(int left, int right) {
-        if(left == 0) return prefix[right];
+        return Query(0 , n-1 , left , right , 0);
+    }
 
-        return prefix[right] - prefix[left-1];
+    public void buildSegmentTree(int i , int left , int right , int[] nums){
+        if(left == right){
+            segment[i] = nums[right];
+            return;
+        }
+        int mid = (left+right)/2;
+        buildSegmentTree(2*i+1 , left , mid , nums);
+        buildSegmentTree(2*i+2 , mid+1 , right , nums);
+        segment[i] = segment[2*i+1] + segment[2*i+2];
+
+    }
+
+    public int Query(int s , int e , int l , int r , int i){
+        if(l > e || r < s) return 0;
+        if(s >= l && e <= r) return segment[i];
+        int mid = (s+e)/2;
+        return Query(s, mid, l, r, 2*i+1) +
+           Query(mid+1, e, l, r, 2*i+2);
     }
 }
 
