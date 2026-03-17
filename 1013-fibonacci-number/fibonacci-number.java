93b82b198c6422611ae9f649a9df612fbec8614f
rya2310 <aryaatole689@gmail.com>
2026-03-17 12:23:35 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (66.28%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,55 @@
+class Solution {
+
+    //Foe loop
+    // time complexcity O(N) spacwe complexcity O(1)
+    // public static int fib(int n) {
+    //     if( n <= 1) return n;
+    //     int p1 = 0;
+    //     int p2 = 1;
+    //     // int curr = 0;
+    //     for(int i = 2; i<= n ; i++){
+    //         int curr = p1 + p2;
+    //         p1 = p2;
+    //         p2 = curr;  
+    //     }
+    //     return p2;
+    // }
+    
+    
+    // Memoization Approach
+    // T(n) = O(N);
+    // public static Integer[] dp = new Integer[46];
+    // public static int fib(int n) {
+    //     if (n <= 1) return n;
+
+    //     if(dp[n] != null) return dp[n];
+
+    //     dp[n] = fib(n-1) + fib(n-2);
+    //     return dp[n];
+    // }
+
+    // Tabulation Approach(Bottom-Up Approach)
+    // public static int fib(int n) {
+    //     if (n <= 1)
+    //         return n;
+    //     int dp[] = new int[n+1];
+    //     dp[0] = 0;
+    //     dp[1] = 1;
+
+    //     for(int i = 2 ; i<= n ; i++){
+    //         dp[i] = dp[i-1] + dp[i - 2];
+    //     }
+    //     return dp[n];
+    // }
+
+    public static int fib(int n) {
+        if(n <= 1) return n;
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2 ; i <= n ; i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+}
