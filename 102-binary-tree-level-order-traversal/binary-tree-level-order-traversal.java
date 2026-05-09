0da68362d904a4c0747d31f145424ab707128abf
rya2310 <aryaatole689@gmail.com>
2026-05-09 09:14:33 +0530

    Time: 1 ms (95.52%) | Memory: 46.5 MB (94.88%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -13,55 +13,52 @@
  *     }
  * }
  */
-// class Solution {
-//     public List<List<Integer>> levelOrder(TreeNode root) {
-//         List<List<Integer>> wraplist = new ArrayList<List<Integer>>();
-//         Queue<TreeNode> queue = new LinkedList<>();
-//         if(root == null) return wraplist;
-
-//         queue.offer(root);
-//         while(!queue.isEmpty()){
-//             int level = queue.size();
-//             List<Integer> sublist = new ArrayList<>();
-//             for(int i = 0; i < level ; i++){
-//                 if(queue.peek().left != null) queue.offer(queue.peek().left);
-//                 if(queue.peek().right != null) queue.offer(queue.peek().right);
-//                 sublist.add(queue.poll().val);
-//             }
-//             wraplist.add(sublist);
-//         }
-//         return wraplist;
-//     }
-
-
-//DFS
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> res = new ArrayList<>();
-        int h = height(root);
-        for(int i = 1 ; i <= h ; i++){
-            List<Integer> level = new ArrayList<>();
-            PrintNode(root , i , level);
-            res.add(level);
-        }
-        return res;    
+        List<List<Integer>> res  = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        if(root == null) return res;
+        q.offer(root);
+        while(!q.isEmpty()){
+            int level = q.size();
+            List<Integer> list = new ArrayList<>();
+            for(int i = 0 ; i < level ; i++){
+                if(q.peek().left != null) q.offer(q.peek().left);
+                if(q.peek().right!= null) q.offer(q.peek().right);
+                list.add(q.poll().val);
+            }
+            res.add(list);
+        }      
+        return res;
     }
+//DFS
+// class Solution {
+//     public List<List<Integer>> levelOrder(TreeNode root) {
+//         List<List<Integer>> res = new ArrayList<>();
+//         int h = height(root);
+//         for(int i = 1 ; i <= h ; i++){
+//             List<Integer> level = new ArrayList<>();
+//             PrintNode(root , i , level);
+//             res.add(level);
+//         }
+//         return res;    
+//     }
 
-    int height(TreeNode root){
-        if(root == null){
-            return 0;
-        }
-        return 1 + Math.max(height(root.left) , height(root.right));
-    }
+//     int height(TreeNode root){
+//         if(root == null){
+//             return 0;
+//         }
+//         return 1 + Math.max(height(root.left) , height(root.right));
+//     }
 
-    void PrintNode(TreeNode root , int lvl , List<Integer>list){
-        if(root == null) return ;
-        if(lvl == 1){
-            list.add(root.val);
-        }else{
-            PrintNode(root.left , lvl-1 , list);
-            PrintNode(root.right , lvl-1 , list);
-        }
-    }
+//     void PrintNode(TreeNode root , int lvl , List<Integer>list){
+//         if(root == null) return ;
+//         if(lvl == 1){
+//             list.add(root.val);
+//         }else{
+//             PrintNode(root.left , lvl-1 , list);
+//             PrintNode(root.right , lvl-1 , list);
+//         }
+//     }
     
 }
