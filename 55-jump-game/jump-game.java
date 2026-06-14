2bdb3d2cfe5b33280c5212f0789a045f66990de7
Arya <aryaatole689@gmail.com>
2026-06-14 17:44:09 +0530

    Time: 2 ms (88.38%) | Memory: 47.9 MB (41.81%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxInd = 0;
+        int n = nums.length;
+        for(int i = 0 ; i < n ; i++){
+            if(i > maxInd) return false;
+            maxInd = Math.max(maxInd , i + nums[i]);
+        }
+        return true;
+    }
+}
