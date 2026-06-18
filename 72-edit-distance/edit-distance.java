0ad2f1fc54e377737f9528ca155d8f25be987a12
Arya <aryaatole689@gmail.com>
2026-06-18 10:54:43 +0530

    Time: 5 ms (69.28%) | Memory: 47.1 MB (70.31%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,24 +1,39 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int[][] dp = new int[word1.length()][word2.length()];
-        for(int[] i : dp){
-            Arrays.fill(i , -1);
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m+1][n+1];
+        for(int i = 0 ; i <= m ; i++){
+            dp[i][0] = i;
         }
-        return solver(0 , 0 , word1 , word2 , dp);
-    }
-    public int solver(int i , int j , String s , String t , int[][] dp){
-        if(i == s.length()) return t.length() -j;
-        if(j == t.length()) return s.length() -i;
-
-        if(dp[i][j] != -1) return dp[i][j];
-        if(s.charAt(i) == t.charAt(j)){
-            return dp[i][j] =  solver(i+1 , j+1 , s , t , dp);
+        for(int i = 0 ; i <= n ; i++){
+            dp[0][i] = i;
         }
-        int insert = solver(i , j+1 , s , t,dp);
-        int delete = solver(i+1 , j , s , t,dp);
-        int replace = solver(i+1 , j+1 , s ,t,dp);
 
-        return dp[i][j] = 1+ Math.min(insert,Math.min(delete , replace));
-
+        for(int i = 1 ; i <= m ; i++){
+            for(int j = 1 ; j <= n ; j++){
+                if(word1.charAt(i-1) == word2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }else{
+                    dp[i][j] = 1 + Math.min(dp[i-1][j] , Math.min(dp[i][j-1], dp[i-1][j-1]));
+                }
+            }
+        }
+        return dp[m][n];
     }
+    // public int solver(int i , int j , String s , String t , int[][] dp){
+    //     if(i == s.length()) return t.length() -j;
+    //     if(j == t.length()) return s.length() -i;
+
+    //     if(dp[i][j] != -1) return dp[i][j];
+    //     if(s.charAt(i) == t.charAt(j)){
+    //         return dp[i][j] =  solver(i+1 , j+1 , s , t , dp);
+    //     }
+    //     int insert = solver(i , j+1 , s , t,dp);
+    //     int delete = solver(i+1 , j , s , t,dp);
+    //     int replace = solver(i+1 , j+1 , s ,t,dp);
+
+    //     return dp[i][j] = 1+ Math.min(insert,Math.min(delete , replace));
+
+    // }
 }
