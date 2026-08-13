4da6d489f332c52847fc408d61a324f6f9e54886
Arya <aryaatole689@gmail.com>
2026-08-13 17:29:46 +0530

    Time: 7 ms (45.98%) | Memory: 41.9 MB (70.75%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -53,16 +53,22 @@ class Solution {
     //     return dp[n];
     // }
 
+    // public static int fib(int n) {
+    //     if(n <= 1) return n; 
+    //     int a = 0;
+    //     int b = 1;
+    //     int c = 0;
+    //     for(int i = 1 ; i < n ; i++){
+    //         c = a+b;
+    //         a = b;
+    //         b = c;
+    //     }
+    //     return c;
+    // }
+
     public static int fib(int n) {
         if(n <= 1) return n; 
-        int a = 0;
-        int b = 1;
-        int c = 0;
-        for(int i = 1 ; i < n ; i++){
-            c = a+b;
-            a = b;
-            b = c;
-        }
-        return c;
+
+        return fib(n-1) + fib(n-2) ;
     }
 }
